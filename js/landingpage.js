e3c6d51ecf7187de02a969409e222158ffbc648e
Aloyise Biju Mathew <aloyise.mathew@experionglobal.local>
2023-12-19 16:15:50 +0530

    fix:completed basic header with login buttons and animations

---
+++ js/landingpage.js
@@ -0,0 +1,9 @@
+const alreadyAccountButtonAnimation = () => {
+    document.getElementById("already-have-account-button").classList.toggle('clicked');
+    setTimeout(() => document.getElementById("already-have-account-button").classList.toggle('clicked'), 300)
+}
+
+const getStartedButtonAnimation = () => {
+    document.getElementById("get-started-button").classList.toggle('clicked');
+    setTimeout(() => document.getElementById("get-started-button").classList.toggle('clicked'), 300)
+}
