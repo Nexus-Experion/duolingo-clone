948a5f10bf0b31df8fd04e5a2f69043b7316f56a
Aloyise Biju Mathew <aloyise.mathew@experionglobal.local>
2024-01-07 22:54:02 +0530

    feat:experimenting with scss

--- js/landingpage.js
+++ js/landingpage.js
@@ -7,12 +7,18 @@ const alreadyAccountButtonAnimation = () => {
 const getStartedButtonAnimation = () => {
     document.getElementById("get-started-button").classList.toggle('clicked');
     setTimeout(() => document.getElementById("get-started-button").classList.toggle('clicked'), 300);
+    animationLoader()
     window.location.href = "/html/languages-select-page.html";
 }
-// const animation = bodymovin.loadAnimation({
-//     container: document.getElementById('left-logging-header-video'),
-//     renderer: 'svg',
-//     loop: true,
-//     autoplay: true,
-//     path: '../assets/json-animations/header.json'
-// });
+const headerPath = '../assets/json-animations/header.json';
+const animationLoader = (id, path) => {
+    bodymovin.loadAnimation({
+        container: document.getElementById(id),
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: path
+    });
+}
+animationLoader("left-logging-header", headerPath)
+
